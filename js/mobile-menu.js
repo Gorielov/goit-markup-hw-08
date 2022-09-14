ad416ad9edebac32b8026d89141b0d5c4380afa2
Gorielov <igor.gorelov.avrora@gmail.com>
2022-09-14 11:33:54 +0300

    finished

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -8,12 +8,13 @@
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    document.body.classList.toggle('scroll-lock');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
+  //   const scrollLockMethod = !isMenuOpen
+  //     ? 'disableBodyScroll'
+  //     : 'enableBodyScroll';
+  //   bodyScrollLock[scrollLockMethod](document.body);
+   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
